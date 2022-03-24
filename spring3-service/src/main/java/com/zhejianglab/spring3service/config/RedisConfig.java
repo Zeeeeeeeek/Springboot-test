55f0a5c7a14d7b782f0ad592c0be87653c41a764
chenze <chenze@zhejianglab.com>
2022-03-24 15:59:36 +0800

    feat: add redis and util

---
+++ spring3-service/src/main/java/com/zhejianglab/spring3service/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.zhejianglab.spring3service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author chenze
+ * @since 2022-03-24
+ */
+@Configuration
+public class RedisConfig {
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        // 配置redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer); // key序列化
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // value序列化
+        redisTemplate.setHashKeySerializer(stringSerializer); // Hash key序列化
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer()); // Hash value序列化
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+
+}
--- spring3-web/src/main/java/com/zhejianglab/spring3web/controller/TestController.java
+++ spring3-web/src/main/java/com/zhejianglab/spring3web/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.zhejianglab.spring3web.controller;
 
 import com.zhejianglab.spring3common.dto.Result;
+import com.zhejianglab.spring3service.redis.RedisUtil;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author chenze
@@ -13,8 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test")
 public class TestController {
 
+    @Resource
+    private RedisUtil redisUtil;
+
     @GetMapping("hello")
     public Result hello() {
         return Result.success("你好");
     }
+
+    @GetMapping("set")
+    public Result set() {
+        redisUtil.set("kk:test:1","ssss",60, TimeUnit.SECONDS);
+        return Result.success("你好");
+    }
+
+    @GetMapping("get")
+    public Result get() {
+        return Result.success(redisUtil.get("kk:test:1"));
+    }
+
 }
